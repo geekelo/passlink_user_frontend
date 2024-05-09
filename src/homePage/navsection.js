b0a2c4e69db3442bf47c636883d2025bae5190b1
geekelo <efueliht12@gmail.com>
2024-05-09 20:58:52 +0100

    Add structure for navsection

---
+++ src/homePage/navsection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavSection() {
+  return (
+    <div className="navsection">
+      <img className="logo" src="" alt="logo" />
+      <div>
+        <p>Mobile icon</p>
+        <p>Close icon</p>
+        <section className="menuSection">
+          <p>page 1</p>
+          <p>page 2</p>
+          <p>page 3</p>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default NavSection;
