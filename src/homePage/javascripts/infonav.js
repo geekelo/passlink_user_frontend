7f03239ca2fed0b069889a78fc0169aabb88f7b0
geekelo <efueliht12@gmail.com>
2024-05-15 13:38:02 +0100

    Add styles for infonav dropdown

--- src/homePage/javascripts/infonav.js
+++ src/homePage/javascripts/infonav.js
@@ -10,6 +10,28 @@ import data from '../data/datalist.json';
 import '../stylesheets/infonav.css';
 
 function InfoNav() {
+  const openDropdown = () => {
+    const openDropdown = document.querySelector('.opendropdown-icon');
+    const closeDropdown = document.querySelector('.closedropdown-icon');
+    const dropdown = document.querySelector('.dropdown-section');
+    openDropdown.classList.add('hide');
+    dropdown.classList.remove('hide');
+    dropdown.classList.add('show');
+    closeDropdown.classList.remove('hide');
+    closeDropdown.classList.add('show');
+  };
+
+  const closeDropdown = () => {
+    const openDropdown = document.querySelector('.opendropdown-icon');
+    const closeDropdown = document.querySelector('.closedropdown-icon');
+    const dropdown = document.querySelector('.dropdown-section');
+    openDropdown.classList.remove('hide');
+    openDropdown.classList.add('show');
+    dropdown.classList.remove('show');
+    dropdown.classList.add('hide');
+    closeDropdown.classList.add('hide');
+  };
+
   return (
     <div className="infoNav-section">
       <div className="back-button">
@@ -20,14 +42,14 @@ function InfoNav() {
       <div className="back-button">
         <div>
           <div>
-            <button type="button" aria-label="nav-open" className="infonav-btn">
+            <button type="button" onClick={openDropdown} aria-label="nav-open" className="infonav-btn opendropdown-icon">
               <FontAwesomeIcon icon={faBars} className="infonav-icons" />
             </button>
-            <button type="button" aria-label="nav-close" className="infonav-btn">
+            <button type="button" onClick={closeDropdown} aria-label="nav-close" className="infonav-btn closedropdown-icon hide">
               <FontAwesomeIcon icon={faTimes} className="infonav-icons" />
             </button>
           </div>
-          <div className="dropdown-section">
+          <div className="dropdown-section hide">
             {data.datalist.map((item) => (
               <NavLink key={item.id} to={item.url} className="infonav-links links">
                 {item.title}
